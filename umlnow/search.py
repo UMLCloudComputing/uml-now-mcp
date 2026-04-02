48fe32db613ec6a5c29adf5301b9096147ee6fa3
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-04-02 14:42:10 -0400

    feat: Renamed and restructured department search to subject prefix search

--- umlnow/search.py
+++ umlnow/search.py
@@ -25,9 +25,9 @@ async def get_html(url):
         await browser.close()
     return html
 
-# Return a total list of classes from a department
-async def get_courses_by_department_prefix(department, parse=False, debug=False):
-    """Return a total list of classes from a department."""
+# Return a total list of classes from a subject
+async def get_courses_by_subject_prefix(subject: str, parse=False, debug=False):
+    """Return a total list of classes from a subject."""
 
     # Output
     OUTPUT = {
@@ -36,7 +36,7 @@ async def get_courses_by_department_prefix(department, parse=False, debug=False)
     }
     
     # Create the url
-    url = f"https://www.uml.edu/Catalog/Advanced-Search.aspx?prefix={department}&type=prefix"
+    url = f"https://www.uml.edu/Catalog/Advanced-Search.aspx?prefix={subject}&type=prefix"
    
     # Get the html response
     html = await get_html(url)
@@ -81,61 +81,60 @@ async def get_courses_by_department_prefix(department, parse=False, debug=False)
     return OUTPUT
 
 
-class Search(object):
-    """Search API. 
-    This object provides the CLI for the search command. This command is used to create a user friendly
-    abstraction for the both the UML Now API and the UML Catalog API."""
+# class Search(object):
+#     """Search API. 
+#     This object provides the CLI for the search command. This command is used to create a user friendly
+#     abstraction for the both the UML Now API and the UML Catalog API."""
     
-    def __init__(self, **params):
-        self.params = params
-        self.debug = True if 'debug' in self.params else False
+#     def __init__(self, **params):
+#         self.params = params
+#         self.debug = True if 'debug' in self.params else False
         
-    def courses(self):
-        """Return a list of classes that match the search criteria."""    
+#     def courses(self):
+#         """Return a list of classes that match the search criteria."""    
         
-        # Output
-        OUTPUT = {
-            'total': 0,
-            'time': time.time(),
-        }
+#         # Output
+#         OUTPUT = {
+#             'total': 0,
+#             'time': time.time(),
+#         }
         
-        # If the departments parameter is set, use it
-        if 'departments' in self.params:
+#         # If the departments parameter is set, use it
+#         if 'departments' in self.params:
             
-            # Is it a string or a tuple?
-            if isinstance(self.params['departments'], str):
-                departments = [self.params['departments']]
-            else:
-                departments = list(self.params['departments'])
+#             # Is it a string or a tuple?
+#             if isinstance(self.params['departments'], str):
+#                 departments = [self.params['departments']]
+#             else:
+#                 departments = list(self.params['departments'])
             
-        # Otherwise, use the entire list of departments
-        else:
-            departments = DEPARTMENT_PREFIXES
+#         # Otherwise, use the entire list of departments
+#         else:
+#             departments = DEPARTMENT_PREFIXES
                         
-        # For department in departments, get the courses
-        for department in departments:
-            if self.debug:
-                print("- Starting search for department: " + department)
-            OUTPUT[department] = get_courses_by_department_prefix(department, parse=('parse' in self.params and self.params['parse']), debug=self.debug)
-            OUTPUT['total'] += OUTPUT[department]['total']
+#         # For department in departments, get the courses
+#         for department in departments:
+#             if self.debug:
+#                 print("- Starting search for department: " + department)
+#             OUTPUT[department] = get_courses_by_department_prefix(department, parse=('parse' in self.params and self.params['parse']), debug=self.debug)
+#             OUTPUT['total'] += OUTPUT[department]['total']
             
-        # Return the output
-        OUTPUT['time'] = time.time() - OUTPUT['time']
-        return OUTPUT
+#         # Return the output
+#         OUTPUT['time'] = time.time() - OUTPUT['time']
+#         return OUTPUT
     
-    def professors(self):
-        """Search professors."""
-        return "Sorry, not implemented yet."
+#     def professors(self):
+#         """Search professors."""
+#         return "Sorry, not implemented yet."
     
-    def majors(self):
-        """Search majors."""
-        return "Sorry, not implemented yet."
+#     def majors(self):
+#         """Search majors."""
+#         return "Sorry, not implemented yet."
     
-    def minors(self):
-        """Search minors."""
-        return "Sorry, not implemented yet."
+#     def minors(self):
+#         """Search minors."""
+#         return "Sorry, not implemented yet."
     
-    def degree_pathways(self):
-        """Search degree pathways."""
-        return "Sorry, not implemented yet."
-    
+#     def degree_pathways(self):
+#         """Search degree pathways."""
+#         return "Sorry, not implemented yet."
