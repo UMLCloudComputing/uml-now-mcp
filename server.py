fed41dfbd97d9603a61785d63207a7f1020a4155
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-04-02 14:43:15 -0400

    feat: Renamed and tested subject prefix search

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from typing import Optional
 from fastmcp import FastMCP, Context
 from fastmcp.server.lifespan import lifespan
 from umlnow import Course, API
-from umlnow import get_courses_by_department_prefix as dpt_prefix
+from umlnow import get_courses_by_subject_prefix as sub_prefix
 
 # Configuration
 BROADCAST_ADDRESS = os.getenv("BROADCAST_ADDRESS", "127.0.0.1")
@@ -15,7 +15,8 @@ async def app_lifespan(server):
   """ Initialization and destruction steps for server """
   print("Starting server...") 
   try:
-    yield
+    # Configure server persistent 
+    yield 
   finally:
     print("Shutting down server...")
     
@@ -39,16 +40,18 @@ async def get_course_info_from_course_id(course_id: str, ctx: Context,
   - course_requirements: Optional boolean. Set to True to get this info
   """
   await ctx.info("Called `get_course_info_from_course_id`")
-  return Course(course_id.strip('\n'), name=course_name, url=course_url, description=course_description, credits=course_credits, requirements=course_requirements)
+  return Course(course_id.strip('\n'), name=course_name, url=course_url, description=course_description, credits=course_credits, requirements_text=course_requirements)
 
-
+# DONE
 @mcp_server.tool
-async def get_courses_by_department_prefix(department: str, ctx: Context, parse: bool = True):
+async def get_all_courses_by_subject_prefix(subject_prefix: str, ctx: Context):
   """
-  Obtain a total list of classes from a department
+  Obtain a total list of classes from a subject
+  - subject_prefix: The subject prefix for the University of Massachusetts Lowell
   """
-  await ctx.info("Called `get_courses_by_department_prefix`")
-  return (await dpt_prefix(department, parse=parse))
+  await ctx.info("Called `get_courses_by_subject_prefix`")
+  result = await sub_prefix(subject=subject_prefix, parse=True)
+  return result
 
 @mcp_server.tool
 async def search_course_title(course_title: str):
@@ -59,3 +62,5 @@ async def search_course_title(course_title: str):
 
 if __name__ == "__main__":
   mcp_server.run(transport="streamable-http", host=BROADCAST_ADDRESS, port=8000)
+
+
